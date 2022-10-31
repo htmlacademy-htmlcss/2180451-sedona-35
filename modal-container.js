f386cc988690129ffb9281cdf5fa484c40ab4d68
Dante01Rus <109906292+Dante01Rus@users.noreply.github.com>
2022-10-31 16:36:24 +0300

    правки

--- modal-container.js
+++ modal-container.js
@@ -3,10 +3,10 @@ let favHotelBtn = document.querySelector(".favorites-hotel-button");
 let modalCloseBtn = document.querySelector(".modal-close-btn");
 
 favHotelBtn.onclick = function () {
-  container.classList.contains("modal-container-hidden")
-  container.classList.remove("modal-container-hidden")
+  container.classList.contains("modal-container-hidden");
+  container.classList.remove("modal-container-hidden");
   }
 
 modalCloseBtn.onclick = function () {
-  container.classList.add("modal-container-hidden")
+  container.classList.add("modal-container-hidden");
 }
