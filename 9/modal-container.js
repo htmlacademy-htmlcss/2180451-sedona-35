2d1b18051f4d562b524bbd87cb8354aeece4858d
Keks <keks@htmlacademy.ru>
2022-11-01 10:40:36 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/modal-container.js
@@ -0,0 +1,12 @@
+let container = document.querySelector(".modal-container");
+let favHotelBtn = document.querySelector(".favorites-hotel-button");
+let modalCloseBtn = document.querySelector(".modal-close-btn");
+
+favHotelBtn.onclick = function () {
+  container.classList.contains("modal-container-hidden");
+  container.classList.remove("modal-container-hidden");
+  }
+
+modalCloseBtn.onclick = function () {
+  container.classList.add("modal-container-hidden");
+}
